544ccc718e269b351e82b33e79862e7983cc999d
chaturyachandra <124783225+chaturyachandra@users.noreply.github.com>
2024-04-24 09:41:30 +0530

    Add files via upload

---
+++ WebFrameworks Classwork/Day7/CW 2/springapp/src/main/java/com/example/springapp/controller/ChaturyaCourseController.java
@@ -0,0 +1,48 @@
+package com.example.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import com.example.springapp.model.ChaturyaCourse;
+import com.example.springapp.service.ChaturyaCourseService;
+
+@RestController
+public class ChaturyaCourseController {
+    @Autowired
+    private ChaturyaCourseService ser;
+
+    @PostMapping("/api/course")
+    public ResponseEntity<ChaturyaCourse> post(@RequestBody ChaturyaCourse person) {
+        if (ser.post(person)) {
+            return new ResponseEntity<>(person, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/api/course")
+    public ResponseEntity<List<ChaturyaCourse>> getAll() {
+        List<ChaturyaCourse> li = ser.getAll();
+        if (li.size() > 0) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/api/course/{courseName}")
+    public ResponseEntity<List<ChaturyaCourse>> getbyAge(@PathVariable String courseName) {
+        List<ChaturyaCourse> li = ser.getbyAge(courseName);
+        if (li.size() > 0) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
