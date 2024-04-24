ed09ca795ebde6f41a0db20d4e8c8a77ddd3d1ec
chaturyachandra <124783225+chaturyachandra@users.noreply.github.com>
2024-04-24 09:43:38 +0530

    Add files via upload

---
+++ WebFrameworks Classwork/Day8/CW 3/springapp/src/main/java/com/example/springapp/controller/ChaturyaStudentController.java
@@ -0,0 +1,48 @@
+package com.example.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import com.example.springapp.model.ChaturyaStudent;
+import com.example.springapp.service.ChaturyaStudentService;
+
+@RestController
+public class ChaturyaStudentController {
+    @Autowired
+    private ChaturyaStudentService studentService;
+
+    @PostMapping("/api/student")
+    public ResponseEntity<ChaturyaStudent> post(@RequestBody ChaturyaStudent student) {
+        if (studentService.post(student)) {
+            return new ResponseEntity<>(student, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/api/student/markGreaterThan/{marks}")
+    public ResponseEntity<List<ChaturyaStudent>> getGreater(@PathVariable int marks) {
+        List<ChaturyaStudent> list = studentService.getGreater(marks);
+        if (list.size() > 0) {
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/api/student/markLessThan/{marks}")
+    public ResponseEntity<List<ChaturyaStudent>> getLesser(@PathVariable int marks) {
+        List<ChaturyaStudent> list = studentService.getLesser(marks);
+        if (list.size() > 0) {
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
